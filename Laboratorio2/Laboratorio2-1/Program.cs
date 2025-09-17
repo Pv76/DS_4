2baca471dd27d920d96dfc9d84ae345ca1948b38
Elpepe <enrique.cen@utp.ac.pa>
2025-09-16 19:49:11 -0500

    Lab2

---
+++ Laboratorio2/Laboratorio2-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Laboratorio2_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+
+        public class MyClass()
+        {
+            public static int Valor;
+        }
+    }
+}
---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+     public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.FirstName = "Pepe";
+            client.LastName = "Aya";
+            client.Age = 15;
+            client.Id = 1;
+
+            Console.WriteLine(client.getFullName());
+            Console.WriteLine("HI");
+            Sumar(1, 2);
+        }
+
+        public static void Sumar(int val1, int val2)
+        {
+            int variableLocal = val1 + val2;
+            Console.WriteLine(variableLocal);
+        }
+    }
+
+    public class Client()
+    {
+        public int Id {  get; set; }
+        public String FirstName {  get; set; }
+        public String LastName { get; set; }
+        public ushort Age { get; set; }
+        public string getFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
