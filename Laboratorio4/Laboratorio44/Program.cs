060a2b47e2999653cb3ae8855a36f274f379a526
Elpepe <enrique.cen@utp.ac.pa>
2025-09-17 20:07:02 -0500

    todo el Lab4

---
+++ Laboratorio4/Laboratorio44/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Laboratorio44
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la nota del estudiante:");
+            float score = float.Parse(Console.ReadLine());
+
+            if(score >= 70)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"SU nota es {score} ha aprobado");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Su note es {score} ha reprobado, debe repetir");
+            }    
+        }
+    }
+}
---
+++ Laboratorio4/Laboratorio45/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Laboratorio45
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int edad1, edad2, edad3, edad4, edad5 = 31;
+
+            edad1 = edad2 = edad3 = edad4 = 32;
+
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}", edad1, edad2, edad3, edad4, edad5);
+        }
+    }
+}
