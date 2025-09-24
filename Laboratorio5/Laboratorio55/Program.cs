02f5581719ec6c3770e358b9d6fb169561da69b5
Elpepe <enrique.cen@utp.ac.pa>
2025-09-23 19:22:12 -0500

    Lab5-5

---
+++ Laboratorio5/Laboratorio55/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Estudiante> estudiantes = new List<Estudiante>
+        {
+            new Estudiante { Nombre = "Juan", Edad = 10 },
+            new Estudiante { Nombre = "Sofía", Edad = 11 },
+            new Estudiante { Nombre = "Ana", Edad = 12 },
+        };
+
+
+        foreach (Estudiante estudiante in estudiantes)
+        {
+            Console.WriteLine("Nombre: " + estudiante.Nombre + ", Edad: " + estudiante.Edad);
+        }
+    }
+
+}
+class Estudiante
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+}
+
