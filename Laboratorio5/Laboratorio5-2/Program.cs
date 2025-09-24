e18ba1bf4571b7193b1a16277d53ceb68a1b9749
Elpepe <enrique.cen@utp.ac.pa>
2025-09-23 19:08:28 -0500

    Lab5-2

---
+++ Laboratorio5/Laboratorio5-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Laboratorio52
+{
+    internal class Program
+    {
+        private int[,] mat;
+        private static void Main(string[] args)
+        {
+            Program ma = new Program();
+            ma.Ingresar();
+            ma.Imprimir();
+        }
+        public void Ingresar()
+        {
+            mat = new int[3, 4];
+            for (int f = 0; f < 3; f++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    Console.Write("Ingrese posicion [" + (f + 1) + "," + (c + 1) + "]: "); string linea;
+                    linea = Console.ReadLine();
+                    mat[f, c] = int.Parse(linea);
+                }
+            }
+        }
+        public void Imprimir()
+        {
+            for (int f = 0; f < 3; f++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    Console.Write(mat[f, c] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
