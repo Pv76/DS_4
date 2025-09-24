5bbc0997f976ec2e6b530b03d4aab7f7f24d3f46
Elpepe <enrique.cen@utp.ac.pa>
2025-09-23 19:11:47 -0500

    Lab5-3

---
+++ Laboratorio5/Laboratorio53/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Laboratorio53
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        { 
+            string[] frutas = { "manzana", "plátano", "naranja" };
+
+            foreach (string fruta in frutas)
+            {
+                Console.WriteLine(fruta);
+            }
+        }
+
+    }
+}
