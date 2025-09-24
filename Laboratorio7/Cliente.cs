098acf3610c4b47511f3ab73eee0b6f1956190c0
Elpepe <enrique.cen@utp.ac.pa>
2025-09-24 18:57:07 -0500

    Lab7

---
+++ Laboratorio7/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Cliente
+{
+    private string nombre;
+    private int monto;
+
+    public Cliente(string name)
+	{
+		nombre = name;
+		monto = 0;
+	}
+
+	public void Depositar(int m)
+	{
+		monto = monto + m;
+	}
+
+	public void Extraer(int m)
+	{
+		monto = monto - m
+	}
+
+	public int getMonto()
+	{
+		return monto;
+	}
+
+	public void Imprimir()
+	{
+		Console.WriteLine(nombre + " tiene depositado la suma de " + monto);
+	}
+}
