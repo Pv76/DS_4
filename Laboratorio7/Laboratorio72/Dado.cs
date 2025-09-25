2f61e83a90acdd958e85e2cfaf9cb4f28d281e3f
Elpepe <enrique.cen@utp.ac.pa>
2025-09-24 20:58:17 -0500

    Lab7-2

---
+++ Laboratorio7/Laboratorio72/Dado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio72
+{
+
+    internal class Dado
+    {
+        private int valor;
+        private static Random aleatorio;
+
+        public void Tirar()
+        {
+            valor = aleatorio.Next(1, 7);
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("El valor del dado es: " + valor);
+        }
+
+        public int getValor()
+        { 
+            return valor;
+        }
+    }
+}
---
+++ Laboratorio7/Laboratorio72/JuegoDeDados.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio72
+{
+    internal class JuegoDeDados
+    {
+        private Dado dado1, dado2, dado3;
+
+        public JuegoDeDados()
+        {
+            {
+                dado1 = new Dado();
+                dado2 = new Dado();
+                dado3 = new Dado();
+            }
+        }
+
+        public void Jugar()
+        {
+            dado1.Tirar();
+            dado1.Imprimir();
+            dado2.Tirar();
+            dado2.Imprimir();
+            dado3.Tirar();
+            dado3.Imprimir();
+
+            if (dado1.getValor() == dado2.getValor() && dado1.getValor() == dado3.getValor())
+            {
+                Console.WriteLine("Gano");
+            }
+            else
+            {
+                Console.WriteLine("Perdio");
+            }
+
+            Console.ReadKey();
+        }   
+    }
+}
