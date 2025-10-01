ae26c99d453fe6390fd9396a56cfa60d2f1491b4
Elpepe <enrique.cen@utp.ac.pa>
2025-09-30 19:25:24 -0500

    Lab8

---
+++ Laboratorio8/Laboratorio8/Trabajador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8
+{
+    class Trabajador : Persona
+    {
+        public int Sueldo;
+
+        Trabajador(string nombre, int edad, string nif, int sueldo)
+            : base(nombre, edad, nif)
+        {
+            Sueldo = sueldo;
+        }
+    }
+}
---
+++ Laboratorio8/Laboratorio82/CuentaCorriente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio82
+{
+    public class CuentaCorriente : Cuenta
+    {
+        public CuentaCorriente(string prmtIdCuenta) : base(prmtIdCuenta)
+        {
+        }
+
+        public override void CalcularIntereses()
+        {
+            System.Console.WriteLine("CuentaCorriente. Calcular Intereses() efectuado para " + "la cuenta {0}", getIdCuenta());
+        }
+    }
+}
