26a985a27963c6244118f211692b7b4c90841142
Elpepe <enrique.cen@utp.ac.pa>
2025-10-02 19:29:46 -0500

    Lab10

---
+++ Laboratorio10/Laboratorio102/Program.vb
@@ -0,0 +1,22 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim radio As Single
+        Dim area As Single
+        Dim circuferencia As Single
+        Const pi = 3.1415926
+
+        Console.Write("Ingrese el radio")
+        radio = Console.ReadLine
+
+        area = pi * radio ^ 2
+        circuferencia = 2 * pi * radio
+
+        Console.WriteLine("El area es: {0}", area)
+        Console.WriteLine("La circunferencia es: {0}", circuferencia)
+
+        Console.ReadKey()
+
+    End Sub
+End Module
