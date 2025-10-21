8dca898adb03aeb1c98bbb4d89853ede336eb98e
Elpepe <enrique.cen@utp.ac.pa>
2025-10-20 19:09:14 -0500

    Proyecto1 inicial

---
+++ Proyecto1/Proyecto1/Form1.cs
@@ -0,0 +1,25 @@
+namespace Proyecto1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
