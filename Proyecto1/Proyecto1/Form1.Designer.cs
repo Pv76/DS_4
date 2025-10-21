19a60ec2baf60449ef436a4f7c439c76c5ba7396
Elpepe <enrique.cen@utp.ac.pa>
2025-10-20 20:01:14 -0500

    Interfaz basica

--- Proyecto1/Proyecto1/Form1.Designer.cs
+++ Proyecto1/Proyecto1/Form1.Designer.cs
@@ -32,19 +32,19 @@
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button7 = new Button();
-            button8 = new Button();
-            button9 = new Button();
-            button10 = new Button();
-            button11 = new Button();
-            button12 = new Button();
-            button13 = new Button();
-            button14 = new Button();
-            button15 = new Button();
-            button16 = new Button();
+            btnEliminar = new Button();
+            btnProd = new Button();
+            btn9 = new Button();
+            btn8 = new Button();
+            btn7 = new Button();
+            btnRestar = new Button();
+            btn6 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            btnSumar = new Button();
+            btn3 = new Button();
+            btn2 = new Button();
+            btn1 = new Button();
             SuspendLayout();
             // 
             // txbResultado
@@ -90,155 +90,167 @@
             button3.Text = "button3";
             button3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnEliminar
             // 
-            button4.Font = new Font("Bahnschrift", 12F);
-            button4.Location = new Point(726, 359);
-            button4.Name = "button4";
-            button4.Size = new Size(230, 90);
-            button4.TabIndex = 4;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
+            btnEliminar.Font = new Font("Bahnschrift", 12F);
+            btnEliminar.Location = new Point(726, 359);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(230, 90);
+            btnEliminar.TabIndex = 4;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
             // 
-            // button5
+            // btnProd
             // 
-            button5.Font = new Font("Bahnschrift", 12F);
-            button5.Location = new Point(726, 455);
-            button5.Name = "button5";
-            button5.Size = new Size(230, 90);
-            button5.TabIndex = 8;
-            button5.Text = "button5";
-            button5.UseVisualStyleBackColor = true;
+            btnProd.Font = new Font("Bahnschrift", 12F);
+            btnProd.Location = new Point(726, 455);
+            btnProd.Name = "btnProd";
+            btnProd.Size = new Size(230, 90);
+            btnProd.TabIndex = 8;
+            btnProd.Text = "x";
+            btnProd.UseVisualStyleBackColor = true;
+            btnProd.Click += btnProd_Click;
             // 
-            // button6
+            // btn9
             // 
-            button6.Font = new Font("Bahnschrift", 12F);
-            button6.Location = new Point(490, 455);
-            button6.Name = "button6";
-            button6.Size = new Size(230, 90);
-            button6.TabIndex = 7;
-            button6.Text = "9";
-            button6.UseVisualStyleBackColor = true;
+            btn9.Font = new Font("Bahnschrift", 12F);
+            btn9.Location = new Point(490, 455);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(230, 90);
+            btn9.TabIndex = 7;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
             // 
-            // button7
+            // btn8
             // 
-            button7.Font = new Font("Bahnschrift", 12F);
-            button7.Location = new Point(254, 455);
-            button7.Name = "button7";
-            button7.Size = new Size(230, 90);
-            button7.TabIndex = 6;
-            button7.Text = "8";
-            button7.UseVisualStyleBackColor = true;
-            button7.Click += button7_Click;
+            btn8.Font = new Font("Bahnschrift", 12F);
+            btn8.Location = new Point(254, 455);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(230, 90);
+            btn8.TabIndex = 6;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
             // 
-            // button8
+            // btn7
             // 
-            button8.Font = new Font("Bahnschrift", 12F);
-            button8.Location = new Point(18, 455);
-            button8.Name = "button8";
-            button8.Size = new Size(230, 90);
-            button8.TabIndex = 5;
-            button8.Text = "7";
-            button8.UseVisualStyleBackColor = true;
+            btn7.Font = new Font("Bahnschrift", 12F);
+            btn7.Location = new Point(18, 455);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(230, 90);
+            btn7.TabIndex = 5;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
             // 
-            // button9
+            // btnRestar
             // 
-            button9.Font = new Font("Bahnschrift", 12F);
-            button9.Location = new Point(726, 551);
-            button9.Name = "button9";
-            button9.Size = new Size(230, 90);
-            button9.TabIndex = 12;
-            button9.Text = "button9";
-            button9.UseVisualStyleBackColor = true;
+            btnRestar.Font = new Font("Bahnschrift", 12F);
+            btnRestar.Location = new Point(726, 551);
+            btnRestar.Name = "btnRestar";
+            btnRestar.Size = new Size(230, 90);
+            btnRestar.TabIndex = 12;
+            btnRestar.Text = "-";
+            btnRestar.UseVisualStyleBackColor = true;
+            btnRestar.Click += btnRestar_Click;
             // 
-            // button10
+            // btn6
             // 
-            button10.Font = new Font("Bahnschrift", 12F);
-            button10.Location = new Point(490, 551);
-            button10.Name = "button10";
-            button10.Size = new Size(230, 90);
-            button10.TabIndex = 11;
-            button10.Text = "6";
-            button10.UseVisualStyleBackColor = true;
+            btn6.Font = new Font("Bahnschrift", 12F);
+            btn6.Location = new Point(490, 551);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(230, 90);
+            btn6.TabIndex = 11;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
             // 
-            // button11
+            // btn5
             // 
-            button11.Font = new Font("Bahnschrift", 12F);
-            button11.Location = new Point(254, 551);
-            button11.Name = "button11";
-            button11.Size = new Size(230, 90);
-            button11.TabIndex = 10;
-            button11.Text = "5";
-            button11.UseVisualStyleBackColor = true;
+            btn5.Font = new Font("Bahnschrift", 12F);
+            btn5.Location = new Point(254, 551);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(230, 90);
+            btn5.TabIndex = 10;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
             // 
-            // button12
+            // btn4
             // 
-            button12.Font = new Font("Bahnschrift", 12F);
-            button12.Location = new Point(18, 551);
-            button12.Name = "button12";
-            button12.Size = new Size(230, 90);
-            button12.TabIndex = 9;
-            button12.Text = "4";
-            button12.UseVisualStyleBackColor = true;
+            btn4.Font = new Font("Bahnschrift", 12F);
+            btn4.Location = new Point(18, 551);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(230, 90);
+            btn4.TabIndex = 9;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
             // 
-            // button13
+            // btnSumar
             // 
-            button13.Font = new Font("Bahnschrift", 12F);
-            button13.Location = new Point(726, 647);
-            button13.Name = "button13";
-            button13.Size = new Size(230, 90);
-            button13.TabIndex = 16;
-            button13.Text = "button13";
-            button13.UseVisualStyleBackColor = true;
+            btnSumar.Font = new Font("Bahnschrift", 12F);
+            btnSumar.Location = new Point(726, 647);
+            btnSumar.Name = "btnSumar";
+            btnSumar.Size = new Size(230, 90);
+            btnSumar.TabIndex = 16;
+            btnSumar.Text = "+";
+            btnSumar.UseVisualStyleBackColor = true;
+            btnSumar.Click += btnSumar_Click;
             // 
-            // button14
+            // btn3
             // 
-            button14.Font = new Font("Bahnschrift", 12F);
-            button14.Location = new Point(490, 647);
-            button14.Name = "button14";
-            button14.Size = new Size(230, 90);
-            button14.TabIndex = 15;
-            button14.Text = "3";
-            button14.UseVisualStyleBackColor = true;
+            btn3.Font = new Font("Bahnschrift", 12F);
+            btn3.Location = new Point(490, 647);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(230, 90);
+            btn3.TabIndex = 15;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
             // 
-            // button15
+            // btn2
             // 
-            button15.Font = new Font("Bahnschrift", 12F);
-            button15.Location = new Point(254, 647);
-            button15.Name = "button15";
-            button15.Size = new Size(230, 90);
-            button15.TabIndex = 14;
-            button15.Text = "2";
-            button15.UseVisualStyleBackColor = true;
+            btn2.Font = new Font("Bahnschrift", 12F);
+            btn2.Location = new Point(254, 647);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(230, 90);
+            btn2.TabIndex = 14;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
             // 
-            // button16
+            // btn1
             // 
-            button16.Font = new Font("Bahnschrift", 12F);
-            button16.Location = new Point(18, 647);
-            button16.Name = "button16";
-            button16.Size = new Size(230, 90);
-            button16.TabIndex = 13;
-            button16.Text = "1";
-            button16.UseVisualStyleBackColor = true;
+            btn1.Font = new Font("Bahnschrift", 12F);
+            btn1.Location = new Point(18, 647);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(230, 90);
+            btn1.TabIndex = 13;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(968, 912);
-            Controls.Add(button13);
-            Controls.Add(button14);
-            Controls.Add(button15);
-            Controls.Add(button16);
-            Controls.Add(button9);
-            Controls.Add(button10);
-            Controls.Add(button11);
-            Controls.Add(button12);
-            Controls.Add(button5);
-            Controls.Add(button6);
-            Controls.Add(button7);
-            Controls.Add(button8);
-            Controls.Add(button4);
+            Controls.Add(btnSumar);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(btnRestar);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btnProd);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(btnEliminar);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
@@ -256,18 +268,18 @@
         private Button button1;
         private Button button2;
         private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
-        private Button button7;
-        private Button button8;
-        private Button button9;
-        private Button button10;
-        private Button button11;
-        private Button button12;
-        private Button button13;
-        private Button button14;
-        private Button button15;
-        private Button button16;
+        private Button btnEliminar;
+        private Button btnProd;
+        private Button btn9;
+        private Button btn8;
+        private Button btn7;
+        private Button btnRestar;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button btnSumar;
+        private Button btn3;
+        private Button btn2;
+        private Button btn1;
     }
 }
--- Proyecto1/Proyecto1/Form1.cs
+++ Proyecto1/Proyecto1/Form1.cs
@@ -17,9 +17,93 @@ namespace Proyecto1
 
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        private void btn1_Click(object sender, EventArgs e)
         {
+            btn_Selec(txbResultado.Text, btn1.Text);
+        }
 
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            btn_Selec(txbResultado.Text, btn2.Text);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            btn_Selec(txbResultado.Text, btn3.Text);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            btn_Selec(txbResultado.Text, btn4.Text);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            btn_Selec(txbResultado.Text, btn5.Text);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            btn_Selec(txbResultado.Text, btn6.Text);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            btn_Selec(txbResultado.Text, btn7.Text);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            btn_Selec(txbResultado.Text, btn8.Text);
+        }
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            btn_Selec(txbResultado.Text, btn9.Text);
+        }
+
+        public void btn_Selec(string val, string num)
+        {
+            if (val == "0")
+            {
+                txbResultado.Text = num;
+            }
+            else
+                txbResultado.Text = val + num;
+
+        }
+
+        public void Eliminar(string val)
+        {
+            if (val == "0")
+            {
+                val = "0";
+            }
+            else if (val.Length == 1)
+            {
+                txbResultado.Text = "0";
+            }
+            else
+                txbResultado.Text = val.Substring(0, val.Length - 1);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            Eliminar(txbResultado.Text.ToString());
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            btn_Selec(txbResultado.Text, btnSumar.Text);
+        }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            btn_Selec(txbResultado.Text, btnRestar.Text);
+        }
+
+        private void btnProd_Click(object sender, EventArgs e)
+        {
+            btn_Selec(txbResultado.Text, btnProd.Text);
         }
     }
 }
